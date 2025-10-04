f9572a59d789909eab80a754d313cc43d8340c40
lscheffel <120077606+lscheffel@users.noreply.github.com>
2025-10-03 23:51:32 -0300

    Add Redis storage URI to Flask Limiter configuration

--- limiter.py
+++ limiter.py
@@ -1,4 +1,6 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from config import Config
 
-limiter = Limiter(key_func=get_remote_address)
+redis_url = f"redis://{Config.REDIS_HOST}:{Config.REDIS_PORT}/{Config.REDIS_DB}"
+limiter = Limiter(key_func=get_remote_address, storage_uri=redis_url)
